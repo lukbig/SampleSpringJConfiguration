5425ce61d38016b173547b8f8d6e7504fdbfa1cc
bigos <mrzolwix@gmail.com>
2017-12-21 22:40:54 +0100

    [TECH] Encapsulate integration tests logic

---
+++ src/test/java/com/bigos/base/MvcIntegrationTests.java
@@ -0,0 +1,27 @@
+package com.bigos.base;
+
+import com.bigos.conf.MvcConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringJUnitConfig(classes = MvcConfig.class)
+@WebAppConfiguration("classpath:com/bigos/conf")
+@ActiveProfiles("test")
+public class MvcIntegrationTests {
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    public MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+}
--- src/test/java/com/bigos/hello/HelloWorldIntegrationTest.java
+++ src/test/java/com/bigos/hello/HelloWorldIntegrationTest.java
@@ -1,33 +1,14 @@
 package com.bigos.hello;
 
-import com.bigos.conf.MvcConfig;
-import org.junit.jupiter.api.BeforeEach;
+import com.bigos.base.MvcIntegrationTests;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringJUnitConfig(classes = MvcConfig.class)
-@WebAppConfiguration("classpath:com/bigos/conf")
-@ActiveProfiles("test")
-class HelloWorldIntegrationTest {
-
-    @Autowired WebApplicationContext webApplicationContext;
-    private MockMvc mockMvc;
-
-    @BeforeEach
-    void setUp() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-    }
+class HelloWorldIntegrationTest extends MvcIntegrationTests {
 
     @Test
     public void shouldLoadSpringContext() throws Exception {
@@ -35,7 +16,9 @@ class HelloWorldIntegrationTest {
                 .perform(MockMvcRequestBuilders.get("/"))
                 .andReturn();
 
-        assertThat(mvcResult.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value());
+        int status = mvcResult.getResponse().getStatus();
+
+        assertThat(status).isEqualTo(HttpStatus.OK.value());
     }
 
     @Test
